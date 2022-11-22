02a974b56cc3d977786cd1903d4bcb3cbbf4c940
sncelta <unket76@gmail.com>
2022-11-22 15:08:08 +0200

    /say: Fix desc, require Manage Messages permission

--- commands/subcommands.js
+++ commands/subcommands.js
@@ -1,9 +1,9 @@
 /* eslint-disable indent */
 /* eslint-disable quotes */
 /* eslint-disable no-mixed-spaces-and-tabs */
-const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
 
-// Make a slash command with a subcommand that sends message to a channel, both specifed by the user.
+// Make a slash command with a subcommand that sends message to a text channel, both specifed by the user.
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,7 +12,7 @@ module.exports = {
     .addSubcommand(subcommand =>
         subcommand
             .setName('channel')
-            .setDescription('Says ')
+            .setDescription('Sends custom message in specified channel')
             .addStringOption(option =>
                 option
                     .setName('text')
@@ -23,9 +23,12 @@ module.exports = {
                 option
                     .setName('target_channel')
                     .setDescription('Channel to send message in.')
+                    // Display only text channels to select
                     .addChannelTypes(ChannelType.GuildText)
                     .setRequired(true),
-            ),
+            )
+    // Make the command be able to executed only if the executor has "Manage Messages" permisson.
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
     ),
 	async execute(interaction) {
 		// Parse user's given channel and set it to a const variable.
